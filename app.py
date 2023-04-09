01bbd14bb17034fa5bd5ce7d2b458b61b94b9614
Utkarsh Verma <utkarshverma@Utkarshs-MacBook-Pro.local>
2023-04-09 19:52:23 +0530

    changed stun server

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from aiortc.contrib.media import MediaRecorder
 from streamlit_webrtc import WebRtcMode, webrtc_streamer
 
 
-st.write('# Apply Canny Filter 😃')
+st.write('# Apply Canny Filter')
 
 url = 'https://7dc9-2405-201-29-8cbf-7d99-7bc1-cccb-885c.ngrok-free.app/yay'
 
@@ -45,6 +45,7 @@ def app():
     webrtc_streamer(
         key="record",
         mode=WebRtcMode.SENDRECV,
+        rtc_configuration={"iceServers": [{"urls": ["stun:stun4.l.google.com:19302"]}]},
         media_stream_constraints={
             "video": {
             "width": {"min": 800, "ideal": 1280}
