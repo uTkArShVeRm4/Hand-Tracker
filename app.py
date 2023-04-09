43e3ec6c3b7d484681c8359810cc8a6759de1652
Utkarsh Verma <utkarshverma@Utkarshs-MacBook-Pro.local>
2023-04-09 19:40:52 +0530

    changed stun server

--- app.py
+++ app.py
@@ -45,7 +45,6 @@ def app():
     webrtc_streamer(
         key="record",
         mode=WebRtcMode.SENDRECV,
-        rtc_configuration={"iceServers": [{"urls": ["stun:stun1.voiceeclipse.net:3478"]}]},
         media_stream_constraints={
             "video": {
             "width": {"min": 800, "ideal": 1280}
